072d13106d7e941804b3474599d499cf4e06952d
dhc8032 <58787680+dhc8032@users.noreply.github.com>
2022-05-23 13:44:44 -0500

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -1,29 +1,61 @@
-let c = document.getElementById("canvas");
-c.width = 1024;
-c.height = 576;
-let set = false;
-const fps = 60;
-let rep;
-let ctx = c.getContext('2d');
-let keyDown = false;
-let keyCode;
-function setup(){
- if (!set){
-//    document.addEventListener('keydown', (event) => {
-//      keyCode = event.code;
-//      keyDown = true;
-//    }, false);
-//    document.addEventListener('keyup', (event) => {
-//      keyDown = false;
-//    }, false);
- 
-   rep = setInterval(update, 1000/fps);
-   set = true;
- }
-}
-function update(){
-    ctx.beginPath();
-    ctx.fillStyle = "#BBADA0";
-    ctx.flilRect(0, 0, c.height, c.height);
-    ctx.closePath();
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>2048</title>
+    <style>
+        /* body{
+            
+        } */
+        #game-container{
+            background-color: #BBADA0;
+            height: 500px;
+            width: 500px;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .game-row{
+            margin-bottom: 15px;
+        }
+        .game-cell{
+            background: rgba(238, 228, 218, 0.35);
+            width: 106.25px;
+            height: 106.25px;
+            margin-right: 15px;
+            border-radius: 3px;
+            float: left;
+        }
+        .game-cell:last-child{
+            margin-right: 0px;
+        }
+    </style>
+</head>
+<body>
+    <div id = "game-container">
+        <div class = "game-row">
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+        </div>
+        <div class = "game-row">
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+        </div>
+        <div class = "game-row">
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+        </div>
+        <div class = "game-row">
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+            <div class = "game-cell"></div>
+        </div>
+    </div>
+</body>
+</html>
